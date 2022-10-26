fa2e4d637fb8f40b351a919858c687252cf862ac
AlexandrSviridenkoTMS09 <91564222+AlexandrSviridenkoTMS09@users.noreply.github.com>
2022-10-26 20:41:36 +0300

    3 part done

--- src/main/java/Task21/MusicPlayer.java
+++ src/main/java/Task21/MusicPlayer.java
@@ -1,15 +1,19 @@
 package Task21;
 
 import Task21.Music;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class MusicPlayer {
-    private Music music;
+    private Music music1;
+    private Music music2;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,
+                       @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+    public String playMusic() {
+        return "Playing: " + music1.getSong() + ", " + music2.getSong();
     }
 }
--- src/main/java/Task21/RockMusic.java
+++ src/main/java/Task21/RockMusic.java
@@ -3,7 +3,6 @@ package Task21;
 import Task21.Music;
 import org.springframework.stereotype.Component;
 
-@Component
 public class RockMusic implements Music {
     @Override
     public String getSong() {
